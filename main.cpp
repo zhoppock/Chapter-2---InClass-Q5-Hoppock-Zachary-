734ce49f11beb002a909125e0972dc376ef1b854
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-26 03:03:14 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//Hoppock, Zachary
+//hello.cpp
+//9/12/2020
+//Outputs the text "Hello World!"
+//Version #1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World!" << endl;
+    return 0;
+}
